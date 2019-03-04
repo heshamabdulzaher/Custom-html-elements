5c27633d13375e432790606b9bc418380333b7b1
hesham <heshamabdulzaher@gmail.com>
2019-03-04 23:23:02 +0200

    Added Outline Input

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-// Standard Input
-const standardInput = document.querySelector(".standard_input input");
+// =============== Standard Input ==============
+const standardInput = document.querySelector("#standardInput");
 standardInput.addEventListener("focus", handleInputsAnimation);
 standardInput.addEventListener("focusout", handleInputsAnimation);
+function handleInputsAnimation(e) {
+  if (e.type === "focus") {
+    e.target.parentElement.classList.add("active");
+  } else if (e.type === "focusout") {
+    e.target.parentElement.classList.remove("active");
+    if (e.target.value) {
+      e.target.parentElement.classList.add("gotValue");
+    } else {
+      e.target.parentElement.classList.remove("gotValue");
+    }
+  }
+}
+// =============== Outline Input ==============
+const outlineInput = document.querySelector("#outlineInput");
+outlineInput.addEventListener("focus", handleInputsAnimation);
+outlineInput.addEventListener("focusout", handleInputsAnimation);
 function handleInputsAnimation(e) {
   if (e.type === "focus") {
     e.target.parentElement.classList.add("active");
